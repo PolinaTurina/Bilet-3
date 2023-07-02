6c73b868ce3c1742c5f0e4b0239f951ae1378a6e
PolinaTurina <polinaturina87@gmail.com>
2023-07-02 14:28:01 +0500

    подготовка проекта

--- shop_app/models.py
+++ shop_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=32, verbose_name='название категории')
+
+    class Meta:
+        verbose_nane = 'Категория'
+        verbose_name_plural = 'Категории'
+    
+    def __str__(self):
+        return self.name
